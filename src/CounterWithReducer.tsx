b56faafae4e2b8137f0664499a281e72e6647203
Yosuke Ishikawa <ishikawa.yosuke@bric.jp>
2020-11-11 14:50:49 +0900

    userReducerの学習

---
+++ src/CounterWithReducer.tsx
@@ -0,0 +1,35 @@
+import React, { useReducer } from 'react';
+
+type StateType = { count: number };
+const initialState: StateType = { count: 0 };
+
+type ActionType = {
+  type: 'decrement' | 'increment' | 'reset';
+};
+
+function reducer(state: StateType, action: ActionType): StateType | never {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    case 'reset':
+      return initialState;
+    default:
+      throw new Error();
+  }
+}
+
+function CounterWithReducer() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <>
+      <p>Count: {state.count}</p>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+    </>
+  );
+}
+
+export default CounterWithReducer;
